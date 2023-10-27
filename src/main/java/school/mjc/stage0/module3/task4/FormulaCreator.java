5fecb59bd1efbfde1d1f5d46e7fbb62513e7fdf9
nurbekkhujaev <nurbek.khuja@gmail.com>
2023-10-27 10:45:51 +0500

    bugs fixed

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        int a, b;
+        double ans;
+
+        ans = (9 * a * a - 5 * b + 2 + a - 7) * ((double) (a + b - 4 * a * b) / 2);
+
+
     }
 }
